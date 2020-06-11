f649430b8b0fbbdfc2b4270a700c1ef6b46673bc
Jen Scrivener <JenScrivener3@gmail.com>
2020-06-11 10:07:05 +1000

    Layout for the game activity

--- app/src/main/java/scrivener/cluewizard/MainActivity.kt
+++ app/src/main/java/scrivener/cluewizard/MainActivity.kt
@@ -2,10 +2,11 @@ package scrivener.cluewizard
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,16 +20,11 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        val mainBtnNew = findViewById<Button>(R.id.mainBtnNew)
+        val mainBtnNew = findViewById<Button>(R.id.main_btn_new)
         mainBtnNew?.setOnClickListener {startActivity<GameActivity>()}
 
-        val mainBtnLoad = findViewById<Button>(R.id.mainBtnLoad)
-        mainBtnLoad?.setOnClickListener {startActivity<LoadActivity>()}
-
-        val mainBtnSettings = findViewById<Button>(R.id.mainBtnSettings)
-        mainBtnSettings?.setOnClickListener {startActivity<SettingsActivity>()}
-
     }
+
 
     private inline fun <reified T: Activity> Activity.startActivity() {
         startActivity(Intent(this, T::class.java))
